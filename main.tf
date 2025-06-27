20434b45198531195c01e118efd5b0ffe1e5cfe8
justrunme <justrunme@gmail.com>
2025-06-27 21:59:05 +0200

    Apply terraform fmt to main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "k8s" {
   source = "./k8s"
 
-  namespace          = var.namespace
-  app_version_blue   = var.app_version_blue
-  app_version_green  = var.app_version_green
+  namespace         = var.namespace
+  app_version_blue  = var.app_version_blue
+  app_version_green = var.app_version_green
 }
